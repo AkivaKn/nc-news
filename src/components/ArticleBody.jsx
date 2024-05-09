3a280dc90e2f7883be391ca53dea0b4581c6b8b0
AkivaKn <akivakaufman@gmail.com>
2024-05-09 15:35:51 +0100

    Voting restrictions
    Only allow logged in users to vote and only on
    other users comments and articles

--- src/components/ArticleBody.jsx
+++ src/components/ArticleBody.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import { patchArticle } from "../api";
 import StyledVotes from "../styling-components/StyledVotes";
 import StyledButton from "../styling-components/StyledButton";
+import { useContext } from "react";
+import { UserContext } from "../contexts/User";
+import { useNavigate } from "react-router-dom";
 
 export default function ArticleBody({
   currentArticle,
@@ -10,48 +13,67 @@ export default function ArticleBody({
 }) {
   const [articleVoteChange, setArticleVoteChange] = useState(0);
   const [isArticlePatchError, setIsArticlePatchError] = useState(false);
+  const { user } = useContext(UserContext);
+  const navigate = useNavigate();
+
   const handleClick = () => {
     setShowComments(!showComments);
   };
 
   const handleVote = (vote) => {
-    setArticleVoteChange((currentVote) => {
-      return currentVote + vote;
-    });
-    patchArticle(currentArticle.article_id, vote)
-      .then(() => {
-        setIsArticlePatchError(false);
-      })
-      .catch(() => {
-        setIsArticlePatchError(true);
-        setArticleVoteChange((currentVote) => {
-          return currentVote - vote;
-        });
+    if (!user || !user.username) {
+      navigate("/login");
+    } else {
+      setArticleVoteChange((currentVote) => {
+        return currentVote + vote;
       });
+      patchArticle(currentArticle.article_id, vote)
+        .then(() => {
+          setIsArticlePatchError(false);
+        })
+        .catch(() => {
+          setIsArticlePatchError(true);
+          setArticleVoteChange((currentVote) => {
+            return currentVote - vote;
+          });
+        });
+    }
   };
 
   return (
     <div id="article-body">
       <p>{currentArticle.body}</p>
-      <StyledVotes>
-      <StyledButton>
-        <button disabled={articleVoteChange === 1} onClick={() => handleVote(1)}>
-          +
-          </button>
+      {user && user.username === currentArticle.author ? null : (
+        <StyledVotes>
+          <StyledButton>
+            <button
+              disabled={articleVoteChange === 1}
+              onClick={() => handleVote(1)}
+            >
+              +
+            </button>
           </StyledButton>
-        <p>{currentArticle.votes + articleVoteChange}</p>
-        <StyledButton>
-        <button disabled={articleVoteChange === -1} onClick={() => handleVote(-1)}>
-          -
-        </button>
-        </StyledButton>
-        {isArticlePatchError ? <p>That didn't work. Please try again.</p> : null}
-      </StyledVotes>
+          <p>{currentArticle.votes + articleVoteChange}</p>
+          <StyledButton>
+            <button
+              disabled={articleVoteChange === -1}
+              onClick={() => handleVote(-1)}
+            >
+              -
+            </button>
+          </StyledButton>
+          {isArticlePatchError ? (
+            <p>That didn't work. Please try again.</p>
+          ) : null}
+        </StyledVotes>
+      )}
+
       <StyledButton>
-      <button onClick={handleClick}>
-        {showComments ? "Hide" : "Show"} {currentArticle.comment_count} comments
+        <button onClick={handleClick}>
+          {showComments ? "Hide" : "Show"} {currentArticle.comment_count}{" "}
+          comments
         </button>
-        </StyledButton>
+      </StyledButton>
     </div>
   );
 }
--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -4,8 +4,9 @@ import StyledButton from "../styling-components/StyledButton";
 import StyledVotes from "../styling-components/StyledVotes";
 import { useContext } from "react";
 import { UserContext } from "../contexts/User";
+import { useNavigate } from "react-router-dom";
 
-export default function CommentCard({ comment}) {
+export default function CommentCard({ comment }) {
   const [commentVoteChange, setCommentVoteChange] = useState(0);
   const [isCommentPatchError, setIsCommentPatchError] = useState(false);
   const [isCommentDeleted, setIsCommentDeleted] = useState(false);
@@ -14,23 +15,27 @@ export default function CommentCard({ comment}) {
   const [isDeleting, setIsDeleting] = useState(false);
   const [commentToPost, setCommentToPost] = useState(comment);
   const { user } = useContext(UserContext);
-  const [articleId,setArticleId] = useState(comment.article_id)
-
+  const [articleId, setArticleId] = useState(comment.article_id);
+  const navigate = useNavigate();
 
   const handleVote = (vote) => {
-    setCommentVoteChange((currentVote) => {
-      return currentVote + vote;
-    });
-    patchComment(commentToPost.comment_id, vote)
-      .then(() => {
-        setIsCommentPatchError(false);
-      })
-      .catch(() => {
-        setIsCommentPatchError(true);
-        setCommentVoteChange((currentVote) => {
-          return currentVote - vote;
-        });
+    if (!user || !user.username) {
+      navigate("/login");
+    } else {
+      setCommentVoteChange((currentVote) => {
+        return currentVote + vote;
       });
+      patchComment(commentToPost.comment_id, vote)
+        .then(() => {
+          setIsCommentPatchError(false);
+        })
+        .catch(() => {
+          setIsCommentPatchError(true);
+          setCommentVoteChange((currentVote) => {
+            return currentVote - vote;
+          });
+        });
+    }
   };
 
   const handleClick = (e) => {
@@ -40,30 +45,32 @@ export default function CommentCard({ comment}) {
     deleteComment(commentToPost.comment_id)
       .then(() => {
         setIsCommentDeleted(true);
-        setIsDeleting(false)
+        setIsDeleting(false);
       })
       .catch(() => {
         e.target.disabled = false;
         setIsCommentDeleted(false);
         setIsCommentDeleteError(false);
-        setIsDeleting(false)
+        setIsDeleting(false);
       });
   };
 
   const handleUndoClick = (e) => {
     e.target.disabled = true;
-    setIsUndoError(false)
+    setIsUndoError(false);
     let newComment = {
       username: user.username,
       body: commentToPost.body,
-      votes: commentToPost.votes
+      votes: commentToPost.votes,
     };
-    postComment(articleId, newComment).then((res) => {
-      setIsCommentDeleted(false);
-      setCommentToPost(res.data.comment)
-    }).catch(() => {
-      setIsUndoError(true)
-    })
+    postComment(articleId, newComment)
+      .then((res) => {
+        setIsCommentDeleted(false);
+        setCommentToPost(res.data.comment);
+      })
+      .catch(() => {
+        setIsUndoError(true);
+      });
   };
 
   return !isCommentDeleted ? (
@@ -72,46 +79,52 @@ export default function CommentCard({ comment}) {
         {commentToPost.author}/ {new Date(comment.created_at).toDateString()}
       </h3>
       <p>{commentToPost.body}</p>
-      <StyledVotes>
-        <StyledButton>
-          <button
-            disabled={commentVoteChange === 1}
-            onClick={() => handleVote(1)}
-          >
-            +
-          </button>
-        </StyledButton>
-        <p>{commentToPost.votes + commentVoteChange}</p>
-        <StyledButton>
-          <button
-            disabled={commentVoteChange === -1}
-            onClick={() => handleVote(-1)}
-          >
-            -
-          </button>
-        </StyledButton>
-        {isCommentPatchError ? (
-          <p>That didn't work. Please try again.</p>
-        ) : null}
-      </StyledVotes>
-
+      {user && user.username === commentToPost.author ? null : (
+        <StyledVotes>
+          <StyledButton>
+            <button
+              disabled={commentVoteChange === 1}
+              onClick={() => handleVote(1)}
+            >
+              +
+            </button>
+          </StyledButton>
+          <p>{commentToPost.votes + commentVoteChange}</p>
+          <StyledButton>
+            <button
+              disabled={commentVoteChange === -1}
+              onClick={() => handleVote(-1)}
+            >
+              -
+            </button>
+          </StyledButton>
+          {isCommentPatchError ? (
+            <p>That didn't work. Please try again.</p>
+          ) : null}
+        </StyledVotes>
+      )}
       {isCommentDeleteError ? <p>That didn't work. Please try again.</p> : null}
       {user && user.username === commentToPost.author ? (
         <>
-        <StyledButton>
-          <button onClick={handleClick}>Delete</button>
-        </StyledButton>
-      {isDeleting ?<div className="deleting-message"><i className="fa-solid fa-spinner fa-spin"></i><p>Deleting</p></div>:null}
-      </>
+          <StyledButton>
+            <button onClick={handleClick}>Delete</button>
+          </StyledButton>
+          {isDeleting ? (
+            <div className="deleting-message">
+              <i className="fa-solid fa-spinner fa-spin"></i>
+              <p>Deleting</p>
+            </div>
+          ) : null}
+        </>
       ) : null}
     </li>
   ) : (
     <>
-        <p>Your comment has been deleted.</p>
-        <StyledButton>
-          <button onClick={handleUndoClick}>Undo</button>
-          </StyledButton>
-        {isUndoError?<p>That didn't work. Please try again.</p>:null}
+      <p>Your comment has been deleted.</p>
+      <StyledButton>
+        <button onClick={handleUndoClick}>Undo</button>
+      </StyledButton>
+      {isUndoError ? <p>That didn't work. Please try again.</p> : null}
     </>
   );
 }
